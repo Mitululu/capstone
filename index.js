2ed0ae2548d1ba126d0a346075e9613f211945c2
Mitul Saha <mituls@andrew.cmu.edu>
2022-04-06 12:52:02 -0400

    trying s3-download module

--- index.js
+++ index.js
@@ -2,37 +2,84 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
+// Load the SDK for JavaScript
+const AWS = require('aws-sdk');
+const downloader = require('s3-download-stream');
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/video", function(req, res){
-    const range = req.headers.range;
-    if(!range){
-        res.status(400).send("Last request missing range header")
-    }
+app.get("/mongo-video", function(req, res){
+    mongodb.MongoClient.connect(url, function(error, client){
+        if(error){
+            res.status(500).json(error);
+            return;
+        }
 
-    const videoPath = "bigbuck.mp4"
-    const videoSize = fs.statSync("bigbuck.mp4").size;
-    
-    //Parsing range here
+        const range = req.headers.range;
+        if(!range){
+            res.status(400).send("Missing required range header");
+        }
 
-    const CHUNK_SIZE = 10 ** 6; // 1MB chunk
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start+CHUNK_SIZE, videoSize-1);
+        const db = client.db('videos');
 
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": end - start + 1,
-        "Content-Type": "video/mp4",
-    };
+        // GridFS Collection
+        db.collection("fs.files").findOne({}, (err, video) => {
+            if(!video){
+                res.status(404).send("No uploaded video...");
+                return;
+            }
 
-    res.writeHead(206, headers);
+            // Response headers
+            const videoSize = video.length;
+            const start = Number(range.replace(/\D/g, ""));
+            const end = videoSize - 1;
 
-    const videoStream = fs.createReadStream(videoPath, {start, end});
-    videoStream.pipe(res);
+            const headers = {
+                "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+                "Accept-Ranges": "bytes",
+                "Content-Length": (end - start + 1),
+                "Content-Type": "video/mp4",
+            };
+
+            res.writeHead(206, headers);
+            
+            const bucket = new mongodb.GridFSBucket(db);
+            const downloadStream = bucket.openDownloadStreamByName('bigbuck', {start});
+
+            downloadStream.pipe(res);
+        });
+    });
 });
+
+// app.get("/video", function(req, res){
+//     const range = req.headers.range;
+//     if(!range){
+//         res.status(400).send("Last request missing range header")
+//     }
+
+//     const videoPath = "bigbuck.mp4"
+//     const videoSize = fs.statSync("bigbuck.mp4").size;
+    
+//     //Parsing range here
+
+//     const CHUNK_SIZE = 10 ** 6; // 1MB chunk
+//     const start = Number(range.replace(/\D/g, ""));
+//     const end = Math.min(start+CHUNK_SIZE, videoSize-1);
+
+//     const headers = {
+//         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+//         "Accept-Ranges": "bytes",
+//         "Content-Length": end - start + 1,
+//         "Content-Type": "video/mp4",
+//     };
+
+//     res.writeHead(206, headers);
+
+//     const videoStream = fs.createReadStream(videoPath, {start, end});
+//     videoStream.pipe(res);
+// });
 
 app.listen(8000, function(){
     console.log("Listening on port 8000...");
